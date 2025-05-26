fef2dedd3ed04e26f4e1cc82315339a6c18c3e04
Pavel Borisov <boripapa21@gmail.com>
2025-05-27 02:13:28 +0300

    Added *almost* working qr-code--promocode system to website. Added IN_DELIVERY to OrderStatus and changed ON_DELIVERY to DELIVERING in CourierStatus

--- src/main/java/com/boripapa/selahidelivery/entity/CourierStatus.java
+++ src/main/java/com/boripapa/selahidelivery/entity/CourierStatus.java
@@ -2,6 +2,6 @@ package com.boripapa.selahidelivery.entity;
 
 public enum CourierStatus {
     AVAILABLE, //Доступен, свободный, на смене
-    ON_DELIVERY, //Занят доставкой
+    DELIVERING, //Занят доставкой
     UNAVAILABLE //Недоступен, занят, не на смене
 }
